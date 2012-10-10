d5c27d9672702ef98a1a2f2a92dd6b59f779f881
Peter <petermclarke@gmail.com>
2012-10-10 13:50:49 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Tutorial from http://www.nodebeginner.org/
+ */
+
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+
+server.start(router.route, handle);
